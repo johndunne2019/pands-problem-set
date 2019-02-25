517346585c1820392fa4c72acaa50b43f838a0b1
John Dunne <G00273895@gmit.ie>
2019-02-25 20:21:27 +0000

    solution_3.py

--- solution_3.py
+++ solution_3.py
@@ -3,9 +3,8 @@
 # This Program prints all the numbers between 1,000 and 10,000 that are divisible by 6 but not by 12 
 
 for i in range (1000, 10000):        # I have defined the range as being between the numbers 1,000 and 10,000
-    if ((i % 6) == 0 and (i % 12) != 0):  # I have used an if statement to capture if the number is divisible by 6 and also not divisible by 12 
+    if (i % 6 == 0) and (i % 12 != 0):  # I have used an if statement to capture if the number is divisible by 6 and also not divisible by 12 
         print(i)   # If both conditions of the if statement above are met for numbers between 1,000 and 10,000 the numbers will be printed to the screen
 
 # I developed this program myself drawing on the information from lectures and I researched online to see if the code was efficient or could be improved
 # I read about using "and" to combine logical statements here and I used this to ask for both conditions in one line: https://www.w3schools.com/python/python_conditions.asp
-# 
