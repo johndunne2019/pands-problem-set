af2e8f84d996a7acca894e5c7264ce697b3bd485
John Dunne <G00273895@gmit.ie>
2019-02-25 21:52:45 +0000

    solution_5.py work in progress

---
+++ solution_5.py
@@ -0,0 +1,7 @@
+# This program asks the user to input a positive integer and tells the user whether or not the number is a prime
+# John Dunne 2019-02-25
+# A prime number is a whole number greater than one thats only factors are one and itself
+# Any number that has factor other than one and itself is not a prime number
+
+x = int(input("Please Enter a Positive Integer"))
+
