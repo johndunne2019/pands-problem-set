5620186a4a9b3040e1bf92e1bdf3371c0f3e595d
John Dunne <G00273895@gmit.ie>
2019-02-27 07:58:06 +0000

    solution_4.py work in progress

---
+++ solution_4.py
@@ -0,0 +1,9 @@
+# This is my solution to solution 4 on the problem set
+# John Dunne 2019-02-27
+# This program asks the user to input any positive integer and then performs a calculation known as Collatz Conjecture
+# The calculation takes the current value and if it is even the value is divided by 2 and if it is odd multiplied by 3 and add 1 
+# With Collatz Conjecture no matter what the starting value is the sequence will always reach 1 
+# This program ends when the current value is 1
+
+x = int(input("Please Enter a Positive Integer"))
+
