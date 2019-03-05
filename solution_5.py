f2355383f7234c597fa4b0d7a45951ff31b98a37
John Dunne <G00273895@gmit.ie>
2019-03-05 19:36:07 +0000

    Solution_5.py

--- solution_5.py
+++ solution_5.py
@@ -5,7 +5,11 @@
 
 x = int(input("Please Enter a Positive Integer:"))  #The user is asked to input a positive integer. The program will then identify if the number is a prime number or not. 
 
-for i in range (2, x):  # A range has been defined between 2 and the number entered by the user and this will be used to check for divisors in the if statement below
-    if x % i == 0:      # the if statement here will loop through all the numbers between 2 and the number that has been inputted by the user (x) and if any of the numbers are evenly divisible into the number inputted it is not a prime number 
-        print ("That is not a Prime Number")  # the statement here will be printed to the screen if the above if statement finds a number in the range that divides the number the user inputted in which case the number is not a prime number 
-        break  # the break statement here will break out of the loop once a the if statement above has been proved through, we dont need to continue looping  as once we have found one divisor we have proved the number entered by the user is not a prime number 
+for i in range (2, x):  # A range has been defined between 2 up to but not including the number entered by the user(x).
+    if x % i == 0:      # the if statement here will loop through all the numbers starting at 2 up to but not including the number that has been inputted by the user (x) and if any of the numbers are evenly divisible into the number inputted it is not a prime number 
+        print (x, "is not a Prime Number")  # the statement here will be printed to the screen if a factor of the integer entered by the user has been found in the range 
+        break  # the break statement here will break out of the loop once a factor of the number x has been found, we dont need to continue looping  as once we have found one divisor we have proved the number entered by the user is not a prime number
+else:    # if the if statement above has executed and has not found any factor for the number then this else statement will be executed and the below line will be printed to the screen
+    print(x, "is a prime number")   #The number is prime if it has no other factors apart from 1 and itself
+
+# This code is adapted from the example we covered in the week 4 lecture on break and continue keywords - https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
