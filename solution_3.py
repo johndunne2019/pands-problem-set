bf0d7eb789f33b1715b8d97d716c800c21c3ee57
John Dunne <G00273895@gmit.ie>
2019-03-08 16:19:05 +0000

    solution_3.py

--- solution_3.py
+++ solution_3.py
@@ -2,8 +2,11 @@
 # John Dunne 2019-02-22
 # This Program prints all the numbers between 1,000 and 10,000 that are divisible by 6 but not by 12 
 
-for i in range (1000, 10000):        # I have defined the range as being between the numbers 1,000 and 10,000
-    if (i % 6 == 0) and (i % 12 != 0):  # I have used an if statement to capture if the number is divisible by 6 and also not divisible by 12 
+print("This Program prints all numbers between 1,000 and 10,000 that are divisible by 6 but not by 12:")
+# I have explained to the user what this program does 
+
+for i in range (1000, 10001):        # I have defined the range as being between the numbers 1,000 and 10,000
+    if (i % 6 == 0) and (i % 12 != 0):  # The if statement here checks if the numbers between 1,000 and 10,000 are divisible by 6 but not by 12
         print(i)   # If both conditions of the if statement above are met for numbers between 1,000 and 10,000 the numbers will be printed to the screen
 
 # I developed this program myself drawing on the information from lectures and I researched online to see if the code was efficient or could be improved
