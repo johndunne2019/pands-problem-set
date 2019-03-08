e9bf87852bd4f7321cbd36cc24f72a182a5d7fab
John Dunne <G00273895@gmit.ie>
2019-03-08 15:39:03 +0000

    solution_7.py

---
+++ solution_7.py
@@ -0,0 +1,8 @@
+# This program takes a positive floating point number as input and outputs an approximation of its square root
+# John Dunne 2019-03-08
+
+print("This program will calculate an approximate square root of any positive floating point number")
+# I have printed this line to the screen in order to give the user some background on what operation will be carried out by this program
+
+x =float(input("Please enter a positive floating point number:"))
+# I have asked the user to enter a positive floating point number and this be assigned the value x for the rest of the program
