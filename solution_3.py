7ad8cffa9c50dc36c239470f44a4d601f2f35bf0
John Dunne <G00273895@gmit.ie>
2019-03-08 16:24:50 +0000

    solution_3.py

--- solution_3.py
+++ solution_3.py
@@ -7,6 +7,7 @@ print("This Program prints all numbers between 1,000 and 10,000 that are divisib
 
 for i in range (1000, 10001):        # I have defined the range as being between the numbers 1,000 and 10,000
     if (i % 6 == 0) and (i % 12 != 0):  # The if statement here checks if the numbers between 1,000 and 10,000 are divisible by 6 but not by 12
+# the if statement is using the modulo function which checks the remainder when the first argument is divided by the second argument 
         print(i)   # If both conditions of the if statement above are met for numbers between 1,000 and 10,000 the numbers will be printed to the screen
 
 # I developed this program myself drawing on the information from lectures and I researched online to see if the code was efficient or could be improved
