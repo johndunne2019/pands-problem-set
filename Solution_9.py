715d699b81846dfc4726cbb41be6d3a30553fe55
John Dunne <G00273895@gmit.ie>
2019-03-08 06:39:18 +0000

    Solution_9.py

---
+++ Solution_9.py
@@ -0,0 +1,5 @@
+# This program reads in a text file from a prompt on the command line and outputs every second line of the text file
+# John Dunne 2019-03-08
+ with open ("filename", "r") as f:
+
+     
