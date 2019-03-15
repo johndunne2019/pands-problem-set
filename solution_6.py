4012f0e4ce98c699381cf88107983e6ba2dadd4a
John Dunne <G00273895@gmit.ie>
2019-03-15 20:17:01 +0000

    Solution 6

--- solution_6.py
+++ solution_6.py
@@ -9,7 +9,7 @@ s = str(input("Please enter a sentence:")).split()[::2]
 # I have called the split with 2 colons to represent the first and last words entered by the user and a step of 2 to print every second word
 # I first wrote the program this way and called the print function straight away but my output was formatted inside square brackets:[Hello, World]
 # I then researched how to use the .join function to join the list back into a string 
-print(*s, sep=' ')
+print(*s, sep= ' ')
 # Here I have asked for the list that was defined in the .split function above to be printed in string format separated by whitespace
 # During my testing of the program I can see punctuation such as quotation marks and commas will be printed to the screen as the separator is this program is set to be whitespace
 # I read how to use the shortcut *s here: https://www.reddit.com/r/learnpython/comments/6b96j3/beginner_how_to_remove_square_bracket_and/
