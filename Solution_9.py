0974dce8e1ddd758a139587c86c1b4af980653ce
John Dunne <G00273895@gmit.ie>
2019-03-15 18:34:37 +0000

    Solution 9 Finished

--- Solution_9.py
+++ Solution_9.py
@@ -1,16 +1,24 @@
 # This program reads in a text file from a prompt on the command line and outputs every second line of the text file
 # John Dunne 2019-03-14
+print("This program is reading every second line of the file called myfile.txt")
+with open ('myfile.txt', 'r') as f:  # with open is used to open the file and I have asked for read mode 
+   count = 0  # count is set to 0 for the first iteration of the for loop
+   for line in f:  # for loop is used to loop through the lines in the file
+      count = count + 1   # 1 is added to the value of count each time the file loops through a line 
+      if count % 2 == 0:  # Modulo operator used to establish if the line is an even numbered line (every second line)
+         print(line)  # Every second line will be printed as output
 
-with open ('myfile.txt', 'r') as f:
-   for line in f:
-      print(line)
-     # I can also use a for loop to print the lines in the file"
-
+# This program is adapted from the reading I completed here: https://stackoverflow.com/questions/30551945/how-do-i-get-python-to-read-only-every-other-line-from-a-file-that-contains-a-po
+# Count is used to assign a starting value of zero to the first line in a file and a for loop will loop through each line of the file
+# Every time the loop runs one is added to the value of count and an if statement with modulo operator will identify even numbered lines 
+# Example - In the first iteration of the for loop the firs line is assigned value of one and thus wont be printed and so on
+# Finally all even numbered lines are printed to the screen as output
 # We covered this material in the week 7 lecture video- "opening files for reading and writing"
 # The with open command is the recommended way to open a file from the command line as the file is closed automatically after operation
 # I read about the with open keyword here: https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # I have asked for the file to be opened in "r" mode which is read only mode 
 
+# The first steps I completed in solving this problem was to create a file to be read and have some text printed to the file
 # Below are the commands I used when setting up the file to be read by this program - myfile.txt
 # First command to write to the text file:
 #with open('myfile.txt', 'w') as f: 
