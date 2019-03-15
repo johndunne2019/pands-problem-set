0c6c5780033bd1f0379fc0d3be946d21571f64a4
John Dunne <G00273895@gmit.ie>
2019-03-15 07:25:14 +0000

    Solution 10 text file

--- Solution_9.py
+++ Solution_9.py
@@ -1,6 +1,7 @@
 # This program reads in a text file from a prompt on the command line and outputs every second line of the text file
 # John Dunne 2019-03-08
-with open ("filename", "r") as f: 
+with open('myfile.txt', 'w') as f: 
+    f.write("This file will be used to run the program in solution 9 on the problem set")
      # I have to save a txt file to be read and return to finish this program to read every second line later
 
 # We covered this material in the week 7 lecture video- "opening files for reading and writing"
