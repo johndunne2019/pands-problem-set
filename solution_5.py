d5d011b91a96cb92fdcdd1013cc0dc484fbbae25
John Dunne <G00273895@gmit.ie>
2019-03-17 19:56:20 +0000

    Solution 6 added curly braces f string

--- solution_5.py
+++ solution_5.py
@@ -8,14 +8,14 @@ print("This program takes a positive integer (whole number) and tells you whethe
 number = int(input("Please Enter a Positive Integer:"))  
 #The user is asked to input a positive integer. The program will then identify if the number is a prime number or not. 
 if number <= 1:    # If the integer entered by the user is less than or equal to one the below prompt will print to the screen
-    print("Prime numbers are greater than one, please enter a positive integer greater than one")
+    print(f"You have entered a negative number {number} Please enter a positive number")
 if number > 1:  # Prime numbers are greater than one and so I have asked for the loop to run only for numbers greater than one
   for i in range (2, number):  # A range has been defined between 2 up to but not including the number entered by the user(x).
     if number % i == 0:      # the if statement here will run within the for loop through all the numbers starting at 2 up to but not including the number that has been inputted by the user (x) and if any of the numbers are evenly divisible into the number inputted it is not a prime number 
-        print ("That is not a Prime")  # the statement here will be printed to the screen if a factor of the integer entered by the user has been found in the range 
+        print (f"{number} That is not a Prime")  # the statement here will be printed to the screen if a factor of the integer entered by the user has been found in the range 
         break  # the break statement here will break out of the loop once a factor of the number x has been found, we dont need to continue looping  as once we have found one divisor we have proved the number entered by the user is not a prime number
   else:    # if the if statement above has executed and has not found any factor for the number then this else statement will be executed and the below line will be printed to the screen
-        print("That is a prime")   #The number is prime if it has no other factors apart from 1 and itself
+        print(f"{number} That is a prime")   #The number is prime if it has no other factors apart from 1 and itself
 
 # This code is adapted from the example we covered in the week 4 lecture on break and continue keywords - https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
 # I have also added a feature whereby if the user enters a number less than or equal to 1 they are prompted to enter only a positive number.
