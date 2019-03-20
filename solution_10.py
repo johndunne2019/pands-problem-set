b525123156b4484663c2d317275142a5554ec47f
John Dunne <G00273895@gmit.ie>
2019-03-20 21:40:22 +0000

    solution 10 work in progress

--- solution_10.py
+++ solution_10.py
@@ -1,2 +1,7 @@
 # This program displays a plot of the functions x, x sqauerd and 2 to the power of x in the [0, 4]
 # John Dunne
+# I am starting to develop this solution while watching the week 9 lecture videos
+import numpy
+
+import matplotlib.pyplot as pl
+# I hwill return to solution
