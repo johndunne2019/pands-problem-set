e3b15476061340eb875fac6020f0d9ebb58c81f9
John Dunne <G00273895@gmit.ie>
2019-03-24 11:57:31 +0000

    Solution 10 work in progress

--- solution_10.py
+++ solution_10.py
@@ -1,7 +1,9 @@
 # This program displays a plot of the functions x, x sqauerd and 2 to the power of x in the [0, 4]
 # John Dunne
 # I am starting to develop this solution while watching the week 9 lecture videos
-import numpy
-
-import matplotlib.pyplot as pl
+# Additional reading matplotlib pyplot tutorial: https://matplotlib.org/users/pyplot_tutorial.html 
+# From the tutorial I see how to plot an axis
+import matplotlib.pyplot as plt # mathplotlib.pyplot is imported and given a shorter name
+plt.plot([0,1,2,3,4])  # x axis is the default so x axis is set in the range 0 to 4 here
+plt.show() # this is the command used to show the plot created above
 # I hwill return to solution
