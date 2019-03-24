d315aa8e5f367309002491c29a2c97eb04559a56
John Dunne <G00273895@gmit.ie>
2019-03-24 18:08:21 +0000

    Solution 10, plot x squared

--- solution_10.py
+++ solution_10.py
@@ -5,15 +5,17 @@
 # From the tutorial I see how to plot an axis
 import numpy as np # numpy module is imported and given the short name np
 import matplotlib.pyplot as pl # mathplotlib.pyplot module is imported and given a shorter name pl
-x = np.arange (0,4) #The range is defined as being between 0 and 4 using the numpy.arange function
-#Using numpy.arange to set the range between 0 and 4, I had previously used the matploblib.plot function
+x = np.arange(start = 0, stop = 4) #The range is defined as being between 0 and 4 using the numpy.arange function
+# Using numpy.arange to set the range between 0 and 4 using start and stop parameters
 pl.xlabel("x axis", fontsize=12, fontweight= 'bold') #adding name to x and y axis (read in pyplot tutorial text section)
 pl.ylabel("y axis", fontsize=12, fontweight= 'bold') #added a font size and font weight to the x and y axis labels
 pl.title("Plot Generated from Solution_10.py", fontsize= 14, fontweight='bold') 
 # Adding a title to the plot and formatting as read in the text section of the pyplot tutorial
-# I have to plot the functions of x, x2 and 2x
-# I have to return later to plot x2 and 2x
-pl.plot(x, c='r', lw= 2.0, ls= '--') #c short for color, ls short for linestyle and lw short for linewidth
-# I formatted the line that will be plotted using the attributes that I read about in the 'controlling line properties' section of the pyplot tutorial
 
+a = x # a is set to the value of x 
+b = x*x # b is set to the value of x squared
+pl.plot(a, c='r', lw= 4.0, ls= '--') # I have asked for a to be plotted with a red line 
+#c short for color, ls short for linestyle and lw short for linewidth
+# I formatted the line that will be plotted using the attributes that I read about in the 'controlling line properties' section of the pyplot tutorial
+pl.plot(b, c='g', lw= 4.0, ls= '--') # I have asked for b to be ploteed with a green line
 pl.show() # this is the command used to show the plot created above
